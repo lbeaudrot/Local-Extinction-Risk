4f4f93c2f1bb82cafb95230cfd3d1336e2bd3f0a
Lydia Beaudrot <lbeaudrot@conservation.org>
2017-01-12 12:36:51 -0800

    Updated for Nature Climate Change results

--- Temperature Plots.R
+++ Temperature Plots.R
@@ -1,3 +1,4 @@
+# Revised for Nature Climate Change top models 
 # Temperature only Plots (excludes species with interaction term in top model)
 
 # Helper functions
@@ -31,15 +32,6 @@ plot.colTvar <- function(inputmodel, inputdata){
     lines(outputdata$Predicted ~ outputdata$Tvar, type="line", lwd=2)
 }
 
-plot.colMean <- function(inputmodel, inputdata){
-    outputdata <- predict(inputmodel, type="col", newdata=inputdata, appendData=TRUE)
-    plot(outputdata$Predicted ~ outputdata$Tmean, pch=19, las=1, ylab="Colonization", xlab="Mean", bty="n", xlim=c(), ylim=c(0,1), cex=0.8, col="white")
-    polygon(c(outputdata$Tmean, rev(outputdata$Tmean)),
-        c(ifelse(outputdata$Predicted + 1.96*outputdata$SE>1, 1, outputdata$Predicted + 1.96*outputdata$SE), 
-          ifelse(rev(outputdata$Predicted - 1.96*outputdata$SE)<0, 0, rev(outputdata$Predicted - 1.96*outputdata$SE))),
-          col="gray75")
-    lines(outputdata$Predicted ~ outputdata$Tmean, type="line", lwd=2)
-}
 
 plot.extTmax <- function(inputmodel, inputdata){
     outputdata <- predict(inputmodel, type="ext", newdata=inputdata, appendData=TRUE)
@@ -71,59 +63,46 @@ plot.extTvar <- function(inputmodel, inputdata){
     lines(outputdata$Predicted ~ outputdata$Tvar, type="line", lwd=2)
 }
 
-plot.extMean <- function(inputmodel, inputdata){
-    outputdata <- predict(inputmodel, type="ext", newdata=inputdata, appendData=TRUE)
-    plot(outputdata$Predicted ~ outputdata$Tmean, pch=19, las=1, ylab="Extinction", xlab="Mean", bty="n", xlim=c(), ylim=c(0,1), cex=0.8, col="white")
-    polygon(c(outputdata$Tmean, rev(outputdata$Tmean)),
-        c(ifelse(outputdata$Predicted + 1.96*outputdata$SE>1, 1, outputdata$Predicted + 1.96*outputdata$SE), 
-          ifelse(rev(outputdata$Predicted - 1.96*outputdata$SE)<0, 0, rev(outputdata$Predicted - 1.96*outputdata$SE))),
-          col="gray75")
-    lines(outputdata$Predicted ~ outputdata$Tmean, type="line", lwd=2)
-}
-
-plot.extTsd <- function(inputmodel, inputdata){
-    outputdata <- predict(inputmodel, type="ext", newdata=inputdata, appendData=TRUE)
-    plot(outputdata$Predicted ~ outputdata$Tsd, pch=19, las=1, ylab="Extinction", xlab="Standard deviation", bty="n", xlim=c(), ylim=c(0,1), cex=0.8, col="white")
-    polygon(c(outputdata$Tsd, rev(outputdata$Tsd)),
-        c(ifelse(outputdata$Predicted + 1.96*outputdata$SE>1, 1, outputdata$Predicted + 1.96*outputdata$SE), 
-          ifelse(rev(outputdata$Predicted - 1.96*outputdata$SE)<0, 0, rev(outputdata$Predicted - 1.96*outputdata$SE))),
-          col="gray75")
-    lines(outputdata$Predicted ~ outputdata$Tsd, type="line", lwd=2)
-    }
 
 
 # Species to model
-# Colonization
-# Caracal aurata (BIF) nms[21]
-    # ~1 ~ Tmin ~1 ~1
-# Cercocebus sanjei (UDZ) nms[11]
-    # ~1 ~ Biotic + Tmin ~1 ~1
-# Cuniculus paca (YAN) nms[40]
-     # ~1 ~Tmin ~Tmin ~1
-# Dasyprocta punctata (VB) nms[2]
-     # ~1 ~Tmin ~1 ~1
-# Paraxerus vexillarius  (UDZ) nms[18]
-     # ~1 ~Tmin ~1 ~1
-# Pecari tajacu (VB_) nms[6]
-     # ~1 ~Tvar+Biotic ~1 ~1
 
+# Colonization
+
+# Pecari tajacu (VB_) nms[6]
+# ~Elevation ~Tvar ~1 ~1
+
+# Potamochoerus larvatus (UDZ) nms[19]
+# ~1 ~Tvar ~1 ~1
+
+# Tragulus kanchil (NAK) nms[54]
+# ~Elevation ~Tmax + Biotic ~1 ~1
+
+# Cercocebus sanjei (UDZ) nms[11]
+# ~1 ~Biotic + Tmin ~1 ~1
+
+# Cercocebus lhoesti (BIF) (UDZ) nms[24]
+# ~Elevation ~Tvar + Biotic ~Tvar + Biotic ~1
 
 #### Extinction 
 
-# Bdeogale crassicauda (UDZ) nms[8]
-    # ~1 ~1 ~Biotic + Tmin ~1
-# Cephalophus nigrifrons (BIF) nms[22]
-    # ~1 ~1 ~Tmean + Tsd ~1
-# Cephalophus spadix (UDZ) nms[10]
-    # ~1 ~1 ~Tvar + Biotic ~1
 # Cricetomys gambianus  (UDZ) nms[13]
-     # ~1 ~1 ~Tmax ~1
-# Cuniculus paca (YAN) nms[40]
-     # ~1 ~Tmin ~Tmin ~1
-# Potamochoerus larvatus (UDZ) nms[19]
-     # ~1 ~1 ~Tvar ~1
-# Tragulus kanchil (NAK) nms[54]
-     # ~1 ~1 ~Tmean + Tsd ~1
+# ~1 ~1 ~Tmax ~1
+
+# Fossa fossana (RNF) nms[56]
+# ~Elevation ~1 ~Tmax ~1
+
+# Pan troglodytes (BIF) nms[27]
+# ~Elevation ~1 ~Tmin ~1
+
+# Bdeogale crassicauda (UDZ) nms[8]
+# ~1 ~1 ~Biotic + Tmin ~1
+
+# Nasua nasua (YAN) nms[46]
+# ~Elevation ~1 ~Biotic + Tmin ~1
+
+# Potamochoerus larvatus (BIF) [29]
+# ~Elevation ~1 ~Tmax + Biotic ~1
 
 
 #### BEGIN PLOTTING ########
@@ -133,279 +112,162 @@ set.panel(4,3)
 par(mar=c(4,4,3,0))
 
 #### Colonization 
-# Caracal aurata (BIF) nms[21]
-    # ~1 ~ Tmin ~1 ~1
-nmsK <- 21
 
-try((fm2.1=colext(psiformula=~1,
-                  gammaformula=~Tmin,
-                  epsilonformula=~1,
-                  pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
+# Pecari tajacu (VB_) nms[6]
+# ~Elevation ~Tvar ~1 ~1
+nmsK <- 6
+fm5.4=colext(psiformula=~Elevation,
+                    gammaformula=~Tvar,
+                    epsilonformula=~1,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm5.4          
+inputdata <- data.frame(Tvar=seq(min(Tvar, na.rm=TRUE),max(Tvar, na.rm=TRUE),length=60))
+plot.colTvar(inputmodel, inputdata)
+mtext("Pecari tajacu (VB)", side=3, line=0, cex=0.7)  
 
-if(exists("fm2.1")) {
-  if(CondNum(fm2.1)<2000){
-    if(CondNum(fm2.1)>0){mods=c(mods,fm2.1)}
-} 
-}}
-inputmodel <- fm2.1          
-inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60))
-plot.colTmin(inputmodel, inputdata)
-mtext("Caracal aurata (BIF)", side=3, line=0, cex=0.7)
+
+# Potamochoerus larvatus (UDZ) nms[19]
+# ~1 ~Tvar ~1 ~1
+nmsK <- 19
+fm5.1=colext(psiformula=~1,
+                    gammaformula=~Tvar,
+                    epsilonformula=~1,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm5.1          
+inputdata <- data.frame(Tvar=seq(min(Tvar, na.rm=TRUE),max(Tvar, na.rm=TRUE),length=60))
+plot.colTvar(inputmodel, inputdata)
+mtext("Potamochoerus larvatus (UDZ)", side=3, line=0, cex=0.7)  
+
+
+# Tragulus kanchil (NAK) nms[54]
+# ~Elevation ~Tmax + Biotic ~1 ~1
+nmsK <- 54
+
+fm9.4=colext(psiformula=~Elevation,
+                    gammaformula=~Tmax + Biotic,
+                    epsilonformula=~1,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm9.4   
+inputdata <- data.frame(Tmax=seq(min(Tmax, na.rm=TRUE),max(Tmax, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
+plot.colTmax(inputmodel, inputdata)
+mtext("Tragulus kanchil (NAK)", side=3, line=0, cex=0.7)
 
 
 # Cercocebus sanjei (UDZ) nms[11]
-    # ~1 ~ Biotic + Tmin ~1 ~1
+# ~1 ~Biotic + Tmin ~1 ~1
 nmsK <- 11
-
-try((fm28.1=colext(psiformula=~1,
-                   gammaformula=~Biotic + Tmin,
-                   epsilonformula=~1,
-                   pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm28.1")) {
-  if(CondNum(fm28.1)<2000){
-    if(CondNum(fm28.1)>0){mods=c(mods,fm28.1)}
-} 
-}}
-inputmodel <- fm28.1
+fm7.4=colext(psiformula=~Elevation,
+                    gammaformula=~Biotic + Tmin,
+                    epsilonformula=~1,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm7.4   
 inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
 plot.colTmin(inputmodel, inputdata)
 mtext("Cercocebus sanjei (UDZ)", side=3, line=0, cex=0.7)
 
 
-# Cuniculus paca (YAN) nms[40]
-     # ~1 ~Tmin ~Tmin ~1
-#nmsK <- 40
-
-#try((fm2=colext(psiformula=~1,
-#                gammaformula=~Tmin,
-#                epsilonformula=~Tmin,
-#                pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-#if(exists("fm2")) {
-#  if(CondNum(fm2)<2000){
-#    if(CondNum(fm2)>0){mods=c(mods,fm2)}
-#} 
-#}}
-#inputmodel <- fm2
-#inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60))
-#plot.colTmin(inputmodel, inputdata)
-#mtext("Cuniculus paca (YAN)", side=3, line=0, cex=0.7)
-
-
-# Dasyprocta punctata (VB) nms[2]
-     # ~1 ~Tmin ~1 ~1
-nmsK <- 2
-
-try((fm2.1=colext(psiformula=~1,
-                  gammaformula=~Tmin,
-                  epsilonformula=~1,
-                  pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm2.1")) {
-  if(CondNum(fm2.1)<2000){
-    if(CondNum(fm2.1)>0){mods=c(mods,fm2.1)}
-} 
-}}
-inputmodel <- fm2.1
-inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60))
-plot.colTmin(inputmodel, inputdata)
-mtext("Dasyprocta punctata (VB)", side=3, line=0, cex=0.7)
-
-
-# Paraxerus vexillarius  (UDZ) nms[18]
-     # ~1 ~Tmin ~1 ~1
-nmsK <- 18
-
-try((fm2.1=colext(psiformula=~1,
-                  gammaformula=~Tmin,
-                  epsilonformula=~1,
-                  pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm2.1")) {
-  if(CondNum(fm2.1)<2000){
-    if(CondNum(fm2.1)>0){mods=c(mods,fm2.1)}
-} 
-}}
-inputmodel <- fm2.1
-inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60))
-plot.colTmin(inputmodel, inputdata)
-mtext("Paraxerus vexillarius  (UDZ)", side=3, line=0, cex=0.7)
+# Cercocebus lhoesti (BIF) nms[24]
+# ~Elevation ~Tvar + Biotic ~Tvar + Biotic ~1
+nmsK <- 24
+fm13.3=colext(psiformula=~Elevation,
+                     gammaformula=~Tvar + Biotic,
+                     epsilonformula=~Tvar + Biotic,
+                     pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm13.3   
+inputdata <- data.frame(Tvar=seq(min(Tvar, na.rm=TRUE),max(Tvar, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
+plot.colTvar(inputmodel, inputdata)
+mtext("Cercocebus lhoesti (BIF)", side=3, line=0, cex=0.7)
 
 
 
 
-
-
-#### Extinction 
-
-
-
-# Bdeogale crassicauda (UDZ) nms[8]
-    # ~1 ~1 ~Biotic + Tmin ~1
-nmsK <- 8
-
-try((fm28.2=colext(psiformula=~1,
-                   gammaformula=~1,
-                   epsilonformula=~Biotic + Tmin,
-                   pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm28.2")) {
-  if(CondNum(fm28.2)<2000){
-    if(CondNum(fm28.2)>0){mods=c(mods,fm28.2)}
-} 
-}}
-inputmodel <- fm28.2
-inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
-plot.extTmin(inputmodel, inputdata)
-mtext("Bdeogale crassicauda (UDZ)", side=3, line=0, cex=0.7)
-
-
-# Cuniculus paca (YAN) nms[40]
-     # ~1 ~Tmin ~Tmin ~1
-#nmsK <- 40
-
-#try((fm2=colext(psiformula=~1,
-#                gammaformula=~Tmin,
-#                epsilonformula=~Tmin,
-#                pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-#if(exists("fm2")) {
-#  if(CondNum(fm2)<2000){
-#    if(CondNum(fm2)>0){mods=c(mods,fm2)}
-#} 
-#}}
-#inputmodel <- fm2
-#inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60))
-#plot.extTmin(inputmodel, inputdata)
-#mtext("Cuniculus paca (YAN)", side=3, line=0, cex=0.7)
-
+#########################
+#### Extinction #########
 
 # Cricetomys gambianus  (UDZ) nms[13]
-     # ~1 ~1 ~Tmax ~1
+# ~1 ~1 ~Tmax ~1
 nmsK <- 13
-
-try((fm6.2=colext(psiformula=~1,
-                  gammaformula=~1,
-                  epsilonformula=~Tmax,
-                  pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm6.2")) {
-  if(CondNum(fm6.2)<2000){
-    if(CondNum(fm6.2)>0){mods=c(mods,fm6.2)}
-} 
-}}
-inputmodel <- fm6.2
+fm3.2=colext(psiformula=~1,
+                    gammaformula=~1,
+                    epsilonformula=~Tmax,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm3.2   
 inputdata <- data.frame(Tmax=seq(min(Tmax, na.rm=TRUE),max(Tmax, na.rm=TRUE),length=60))
 plot.extTmax(inputmodel, inputdata)
 mtext("Cricetomys gambianus (UDZ)", side=3, line=0, cex=0.7)
 
 
+# Fossa fossana (RNF) nms[56]
+# ~Elevation ~1 ~Tmax ~1
+nmsK <- 56
+fm3.5=colext(psiformula=~Elevation,
+                    gammaformula=~1,
+                    epsilonformula=~Tmax,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm3.5   
+inputdata <- data.frame(Tmax=seq(min(Tmax, na.rm=TRUE),max(Tmax, na.rm=TRUE),length=60))
+plot.extTmax(inputmodel, inputdata)
+mtext("Fossa fossana (RNF)", side=3, line=0, cex=0.7)
 
-# Pecari tajacu (VB_) nms[6]
-     # ~1 ~Tvar+Biotic ~1 ~1
-nmsK <- 6
-
-try((fm42.1=colext(psiformula=~1,
-                   gammaformula=~Tvar + Biotic,
-                   epsilonformula=~1,
-                   pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm42.1")) {
-  if(CondNum(fm42.1)<2000){
-    if(CondNum(fm42.1)>0){mods=c(mods,fm42.1)}
-} 
-}}
-inputmodel <- fm42.1
-inputdata <- data.frame(Tvar=seq(min(Tvar, na.rm=TRUE),max(Tvar, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
-plot.colTvar(inputmodel, inputdata)
-mtext("Pecari tajacu (VB)", side=3, line=0, cex=0.7)
+# Pan troglodytes (BIF) nms[27]
+# ~Elevation ~1 ~Tmin ~1
+nmsK <- 27
+fm2.5=colext(psiformula=~Elevation,
+                    gammaformula=~1,
+                    epsilonformula=~Tmin,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm2.5   
+inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60))
+plot.extTmin(inputmodel, inputdata)
+mtext("Pan troglodytes (BIF)", side=3, line=0, cex=0.7)
 
 
-# Cephalophus spadix (UDZ) nms[10]
-    # ~1 ~1 ~Tvar + Biotic ~1
-nmsK <- 10
+# Bdeogale crassicauda (UDZ) nms[8]
+# ~1 ~1 ~Biotic + Tmin ~1
+nmsK <- 8
+fm7.2=colext(psiformula=~1,
+                    gammaformula=~1,
+                    epsilonformula=~Biotic + Tmin,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm7.2   
+inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
+plot.extTmin(inputmodel, inputdata)
+mtext("Bdeogale crassicauda (UDZ)", side=3, line=0, cex=0.7)
 
-try((fm42.2=colext(psiformula=~1,
-                   gammaformula=~1,
-                   epsilonformula=~Tvar + Biotic,
-                   pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
+# Nasua nasua (YAN) nms[46]
+# ~Elevation ~1 ~Biotic + Tmin ~1
+nmsK <- 46
+fm7.5=colext(psiformula=~Elevation,
+                    gammaformula=~1,
+                    epsilonformula=~Biotic + Tmin,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm7.5   
+inputdata <- data.frame(Tmin=seq(min(Tmin, na.rm=TRUE),max(Tmin, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
+plot.extTmin(inputmodel, inputdata)
+mtext("Nasua nasua (YAN)", side=3, line=0, cex=0.7)
 
-if(exists("fm42.2")) {
-  if(CondNum(fm42.2)<2000){
-    if(CondNum(fm42.2)>0){mods=c(mods,fm42.2)}
-} 
-}}
-inputmodel <- fm42.2
+# Potamochoerus larvatus (BIF) [29]
+# ~Elevation ~1 ~Tmax + Biotic ~1
+nmsK <- 29
+fm9.5=colext(psiformula=~Elevation,
+                    gammaformula=~1,
+                    epsilonformula=~Tmax + Biotic,
+                    pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm9.5   
+inputdata <- data.frame(Tmax=seq(min(Tmax, na.rm=TRUE),max(Tmax, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
+plot.extTmax(inputmodel, inputdata)
+mtext("Potamochoerus larvatus (BIF)", side=3, line=0, cex=0.7)
+
+# Cercocebus lhoesti (BIF) nms[24]
+# ~Elevation ~Tvar + Biotic ~Tvar + Biotic ~1
+nmsK <- 24
+fm13.3=colext(psiformula=~Elevation,
+                     gammaformula=~Tvar + Biotic,
+                     epsilonformula=~Tvar + Biotic,
+                     pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))
+inputmodel <- fm13.3   
 inputdata <- data.frame(Tvar=seq(min(Tvar, na.rm=TRUE),max(Tvar, na.rm=TRUE),length=60), Biotic=rep(0, length=60))
 plot.extTvar(inputmodel, inputdata)
-mtext("Cephalophus spadix (UDZ)", side=3, line=0, cex=0.7)
-
-
-# Potamochoerus larvatus (UDZ) nms[19]
-     # ~1 ~1 ~Tvar ~1
-nmsK <- 19
-
-try((fm7.2=colext(psiformula=~1,
-                  gammaformula=~1,
-                  epsilonformula=~Tvar,
-                  pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm7.2")) {
-  if(CondNum(fm7.2)<2000){
-    if(CondNum(fm7.2)>0){mods=c(mods,fm7.2)}
-} 
-}}
-inputmodel <- fm7.2
-inputdata <- data.frame(Tvar=seq(min(Tvar, na.rm=TRUE),max(Tvar, na.rm=TRUE),length=60))
-plot.extTvar(inputmodel, inputdata)
-mtext("Potamochoerus larvatus (UDZ)", side=3, line=0, cex=0.7)
-
-
-# Cephalophus nigrifrons (BIF) nms[22]
-    # ~1 ~1 ~Tmean + Tsd ~1
-nmsK <- 22
-
-try((fm3.2=colext(psiformula=~1,
-                  gammaformula=~1,
-                  epsilonformula=~Tmean + Tsd,
-                  pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm3.2")) {
-  if(CondNum(fm3.2)<2000){
-    if(CondNum(fm3.2)>0){mods=c(mods,fm3.2)}
-} 
-}}
-inputmodel <- fm3.2
-#inputdata <- data.frame(Tmean=seq(min(Tmean, na.rm=TRUE),max(Tmean, na.rm=TRUE),length=60), Tsd=rep(0, length=60))
-#plot.extMean(inputmodel, inputdata)
-inputdata <- data.frame(Tsd=seq(min(Tsd, na.rm=TRUE),max(Tsd, na.rm=TRUE),length=60), Tmean=rep(0, length=60))
-plot.extTsd(inputmodel, inputdata)
-mtext("Cephalophus nigrifrons (BIF)", side=3, line=0, cex=0.7)
-
-
-
-
-
-# Tragulus kanchil (NAK) nms[54]
-     # ~1 ~1 ~Tmean + Tsd ~1
-nmsK <- 54
-
-try((fm3.2=colext(psiformula=~1,
-                  gammaformula=~1,
-                  epsilonformula=~Tmean + Tsd,
-                  pformula=~1,data=umf,method="L-BFGS-B",control=list(maxit=20000))),silent=TRUE)
-
-if(exists("fm3.2")) {
-  if(CondNum(fm3.2)<2000){
-    if(CondNum(fm3.2)>0){mods=c(mods,fm3.2)}
-} 
-}}
-inputmodel <- fm3.2
-#inputdata <- data.frame(Tmean=seq(min(Tmean, na.rm=TRUE),max(Tmean, na.rm=TRUE),length=60), Tsd=rep(0, length=60))
-#plot.extMean(inputmodel, inputdata)
-inputdata <- data.frame(Tsd=seq(min(Tsd, na.rm=TRUE),max(Tsd, na.rm=TRUE),length=60), Tmean=rep(0, length=60))
-plot.extTsd(inputmodel, inputdata)
-mtext("Tragulus kanchil (NAK)", side=3, line=0, cex=0.7)
+mtext("Cercocebus lhoesti (BIF)", side=3, line=0, cex=0.7)
 
 
